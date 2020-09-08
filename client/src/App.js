9da03dd6245e0f2a3f77e061fd190405127cd40d
dirtTastesGood <keegood8@gmail.com>
2020-09-08 15:56:33 -0700

    added alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alerts/AlertState';
 
 import './App.css';
 
@@ -12,24 +13,28 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-          </Fragment>
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home}></Route>
-              <Route exact path='/about' component={About}></Route>
-              <Route exact path='/register' component={Register}></Route>
-              <Route exact path='/login' component={Login}></Route>
-            </Switch>
-          </div>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+            </Fragment>
+            <div className='container'>
+              <Alerts />
+              <Switch>
+                <Route exact path='/' component={Home}></Route>
+                <Route exact path='/about' component={About}></Route>
+                <Route exact path='/register' component={Register}></Route>
+                <Route exact path='/login' component={Login}></Route>
+              </Switch>
+            </div>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+
+import AlertContext from '../../context/alerts/alertContext';
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -15,7 +21,15 @@ const Register = () => {
   const onSubmit = e => {
     e.preventDefault();
 
-    console.log('Register submit');
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields.', 'danger');
+    }
+
+    if (password !== password2) {
+      setAlert('Passwords do not match.', 'danger');
+    } else {
+      console.log('Register submit');
+    }
 
     // Call register
   };
@@ -28,11 +42,23 @@ const Register = () => {
       <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
-          <input type='text' name='name' value={name} onChange={onChange} />
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' value={email} onChange={onChange} />
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
@@ -41,6 +67,8 @@ const Register = () => {
             name='password'
             value={password}
             onChange={onChange}
+            minLength='6'
+            required
           />
         </div>
         <div className='form-group'>
@@ -50,6 +78,8 @@ const Register = () => {
             name='password2'
             value={password2}
             onChange={onChange}
+            minLength='6'
+            required
           />
         </div>
         <input
