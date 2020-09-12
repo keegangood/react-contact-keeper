ba16f60d0fc236b589fa0718d6f54ad632b5af94
dirtTastesGood <keegood8@gmail.com>
2020-09-12 14:58:58 -0700

    login user

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -23,6 +23,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../types';
 
 if (localStorage.token) {
@@ -115,7 +116,7 @@ const AuthState = props => {
   };
 
   // Logout
-  const logout = () => console.log('logout user');
+  const logout = () => dispatch({ type: LOGOUT });
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
